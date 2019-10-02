5c11d169450529eeaf65af23e1d3d9dc761e71e9
zanomate-desktop <zanomate@gmail.com>
2019-10-02 16:23:29 +0200

    @typescript-eslint/eslint-plugin configurations

--- rules/base.js
+++ rules/base.js
@@ -1,14 +1,22 @@
 module.exports = {
   rules: {
-    'semi': ['warn', 'never'],
-    'indent': ['warn', 2],
+    'brace-style': 'off',
+    'camelcase': 'off',
+    'class-methods-use-this': ['off'],
+    'func-call-spacing': 'off',
+    'indent': ['off'],
+    'jsx-quotes': ['error', 'prefer-double'],
+    'no-empty-function': 'off',
+    'no-empty-interface': 'off',
+    'no-extra-parens': 'off',
     'no-underscore-dangle': ['error', {
       'allow': ['__REDUX_DEVTOOLS_EXTENSION_COMPOSE__'],
       'allowAfterThis': false,
       'allowAfterSuper': false,
       'enforceInMethodNames': true
     }],
-    'jsx-quotes': ['error', 'prefer-double'],
-    'class-methods-use-this': ['off']
+    'quotes': 'off',
+    'require-await': 'off',
+    'semi': ['warn', 'never'],
   }
 }
