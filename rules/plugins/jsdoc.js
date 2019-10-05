ceddd393cb9fed5444b286f1a134c1d1950306c8
zanomate-desktop <zanomate@gmail.com>
2019-10-05 10:46:12 +0200

    jdoc optimized for TS

--- rules/plugins/jsdoc.js
+++ rules/plugins/jsdoc.js
@@ -6,43 +6,33 @@ module.exports = {
   ],
   rules: {
     'jsdoc/check-alignment': 'warn',
-    'jsdoc/check-examples': ['error', {
+    'jsdoc/check-examples': ['warn', {
       'captionRequired': true,
       'paddedIndent': 2
     }],
-    'jsdoc/check-indentation': 'off',
+    'jsdoc/check-indentation': 'warn',
     'jsdoc/check-param-names': 'warn',
     'jsdoc/check-syntax': 'warn',
-    'jsdoc/check-tag-names': ['warn', {
-      'definedTags': ['test']
-    }],
-    'jsdoc/check-types': 'error',
-    'jsdoc/implements-on-classes': 'error',
+    'jsdoc/check-tag-names': 'warn',
+    'jsdoc/check-types': 'off',
+    'jsdoc/implements-on-classes': 'off',
     'jsdoc/match-description': 'off',
-    'jsdoc/newline-after-description': ['warn', 'always'],
-    'jsdoc/no-types': 'off',
+    'jsdoc/newline-after-description': 'off',
+    'jsdoc/no-types': 'warn',
     'jsdoc/no-undefined-types': 'warn',
     'jsdoc/require-description': 'warn',
     'jsdoc/require-description-complete-sentence': 'warn',
     'jsdoc/require-example': 'off',
-    'jsdoc/require-hyphen-before-param-description': ['error', 'never'],
-    'jsdoc/require-jsdoc': ['warn', {
-      'require': {
-        'ArrowFunctionExpression': true,
-        'ClassDeclaration': true,
-        'ClassExpression': true,
-        'FunctionDeclaration': true,
-        'MethodDefinition': false
-      }
-    }],
+    'jsdoc/require-hyphen-before-param-description': ['warn', 'never'],
+    'jsdoc/require-jsdoc': 'off',
     'jsdoc/require-param': 'warn',
-    'jsdoc/require-param-description': 'warn',
+    'jsdoc/require-param-description': 'off',
     'jsdoc/require-param-name': 'warn',
-    'jsdoc/require-param-type': 'warn',
+    'jsdoc/require-param-type': 'off',
     'jsdoc/require-returns': 'warn',
-    'jsdoc/require-returns-check': 'warn',
-    'jsdoc/require-returns-description': 'warn',
-    'jsdoc/require-returns-type': 'warn',
+    'jsdoc/require-returns-check': 'off',
+    'jsdoc/require-returns-description': 'off',
+    'jsdoc/require-returns-type': 'off',
     'jsdoc/valid-types': 'error'
   }
 }
